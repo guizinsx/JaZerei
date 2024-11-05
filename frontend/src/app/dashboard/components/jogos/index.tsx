554e45761afc320d39d4b96a92d73a464980f465
guizinsx <guizin51@gmail.com>
2024-11-05 18:38:24 -0300

    feat: creation os dashboard page and buttons

--- frontend/src/app/dashboard/components/jogos/index.tsx
+++ frontend/src/app/dashboard/components/jogos/index.tsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss'
+import { RefreshCw } from 'lucide-react'
 
 export function Jogos(){
     return(
@@ -6,6 +7,19 @@ export function Jogos(){
 
             <section className={styles.containerHeader}>
                 <h1>Lista de jogos: </h1>
+                <button>
+                    <RefreshCw size={24} color="#3fffa3" />
+                </button>
+            </section>
+
+            <section className={styles.popularGames}>
+                <button
+                    className={styles.jogosItem}
+                >
+                    <div className={styles.tag}></div>
+                    <span> Jogo popular 1</span>
+                </button>
+
             </section>
             
         </main>
